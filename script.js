3ccf756d83d6b2f7d9553dceb460e5dd5f55dedb
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2020-06-02 05:07:48 +0200

    finished; carousel is still  missing and navbar needs to be improved

--- script.js
+++ script.js
@@ -1,21 +1,62 @@
 /* VARIABLES */
-var aboutHeight = document.getElementById('aboutSection').scrollHeight;
+
+/* FUNCTIONS */
+
+// smooth scrolling navigation
+const smoothScroll = () => {
+  const navLinks = document.querySelectorAll(".navbar-item");
+  for (let n in navLinks) {
+    if (navLinks.hasOwnProperty(n)) {
+      navLinks[n].addEventListener("click", e => {
+        e.preventDefault();
+        document.querySelector(navLinks[n].hash).scrollIntoView({
+          behavior: "smooth"
+        });
+      });
+    }
+  }
+}
+smoothScroll();
+// scrollspy
+const scrollSpy = () => {
+  const sections = document.getElementsByTagName("article");
+
+  window.onscroll = () => {
+    const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+    for (var s in sections) {
+      if (sections.hasOwnProperty(s) && sections[s].offsetTop <= scrollPosition) {
+        const id = sections[s].id;
+        document.querySelector(".active-item").classList.remove(".active-item");
+
+        document.querySelector('a[href*=${id}]').parentNode.classList.add(".active-item");
+      }
+    }
+  }
+}
+scrollSpy();
+
+// collapsible navbar
+const responsiveNavbar = () => {
+  document.getElementsByClassName("hamburger-icon")[0].addEventListener("click", e => {
+    if (document.getElementById("headerNavbar").className === "navbar-content") {
+      document.getElementById("headerNavbar").className += " responsive";
+    } else {
+      document.getElementById("headerNavbar").className = "navbar-content";
+    }
+  });
+}
+responsiveNavbar();
+
 
 /* EVENT LISTENERS */
 
-// scroll
-window.addEventListener("scroll", function(e) {
-  currentPosition = window.scrollY;
-  if (currentPosition >= aboutHeight) {
-    alert("about section!");
-    /*
-    document.getElementById("navbarSection").style.background = "#333";
-    document.getElementById("navbarSection").style.transition = "all 0.5s";
-    */
+// shrink navigation menu on scroll
+window.addEventListener("scroll", e => {
+
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("logo").style.cssText = "width: calc(7em + 1.5vw); transition: all 2s ease-out";
   } else {
-    /*
-    document.getElementById("navbarSection").style.background = "transparent";
-    document.getElementById("navbarSection").style.transition = "all 0.5s";
-    */
+    document.getElementById("logo").style.cssText = "width: calc(10em + 0.5vw); transition: all 2s ease-out";
   }
 });
